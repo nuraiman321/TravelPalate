d034b515dd444d1e1c4cf7cc8a62c648435e708e
nuraiman321 <nuraiman.gdb@gmail.com>
2024-03-23 23:07:33 +0800

    remove unnecessary padding

--- app/page.tsx
+++ app/page.tsx
@@ -255,7 +255,7 @@ export default function Home() {
     fetchState();
   }, []);
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="flex flex-col items-center justify-center gap-4  md:py-10">
       <div className="grid w-full gap-2">
         {/* <Input
           aria-label="Search"
